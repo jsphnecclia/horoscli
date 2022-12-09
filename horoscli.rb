a5938c8fd45469adcb292cfc3acf3583f75dff6b
Parker McGowan <jparkermcgowan@gmail.com>
2022-12-08 21:04:06 -0600

    horoscli AGPL

--- horoscli.rb
+++ horoscli.rb
@@ -1,4 +1,20 @@
 #!/usr/bin/env ruby
+#
+# This file is part of horoscli.
+#
+# Copyright (c) 2022 Parker McGowan
+# 
+# Horoscli is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# Horoscli is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with horoscli.  If not, see <https://www.gnu.org/licenses/>.
+
 require_relative 'lib/ruephbase.rb'
 require_relative 'lib/rueph.rb'
 require_relative 'lib/horoscli.rb'
